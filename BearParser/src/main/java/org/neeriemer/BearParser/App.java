5607de16c307307942a9b9565d275afe5094e779
Troy Neeriemer <tneer@yahoo-inc.com>
2013-10-24 18:41:55 -0500

    Changes at work

--- BearParser/src/main/java/org/neeriemer/BearParser/App.java
+++ BearParser/src/main/java/org/neeriemer/BearParser/App.java
@@ -2,6 +2,7 @@ package org.neeriemer.BearParser;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.stream.JsonGenerator;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -9,57 +10,64 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.glassfish.jersey.jsonp.JsonProcessingFeature;
+
 public class App {
 
-	private static final String TRELLO_KEY = "d2d8c722a7238bafb3cb18c1c07bddb8";
-	private static final String TRELLO_TOKEN = "c529cf50dddc74f01a36b1898dc98a1530bb1020bd1e10eb4dbd82e893b56b0c";
+    private static final String TRELLO_KEY = "d2d8c722a7238bafb3cb18c1c07bddb8";
+    private static final String TRELLO_TOKEN = "c529cf50dddc74f01a36b1898dc98a1530bb1020bd1e10eb4dbd82e893b56b0c";
 
-	private static final String DEN_ORG_NAME = "beartest";
+    private static final String DEN_ORG_NAME = "beartest";
 
-	public static void main(String[] args) {
-		System.out.println("Starting...");
-		try {
-			// List<Category> categories = Category.buildCategoryList(new File(
-			// "req.html"));
-			// for (Category cat : categories) {
-			// System.out.println(cat);
-			// for (Achievement ach : cat.getAchievements()) {
-			// System.out.println(ach);
-			// for (Requirement req : ach.getRequirements()) {
-			// System.out.println(req);
-			// }
-			// }
-			// }
+    public static void main(String[] args) {
+        System.out.println("Starting...");
+        try {
+            // List<Category> categories = Category.buildCategoryList(new File(
+            // "req.html"));
+            // for (Category cat : categories) {
+            // System.out.println(cat);
+            // for (Achievement ach : cat.getAchievements()) {
+            // System.out.println(ach);
+            // for (Requirement req : ach.getRequirements()) {
+            // System.out.println(req);
+            // }
+            // }
+            // }
 
-			trello("James");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            trello("James2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		System.out.println("Done.");
-	}
+        System.out.println("Done.");
+    }
 
-	private static void trello(final String boardName) {
+    private static void trello(final String boardName) {
 
-		JsonObject model = Json.createObjectBuilder().add("name", boardName)
-				.add("idOrganization", DEN_ORG_NAME).build();
+        JsonObject model = Json.createObjectBuilder().add("name", boardName).add("idOrganization", DEN_ORG_NAME)
+                                        .build();
 
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(buildUrl("boards"));
+        Client client = ClientBuilder.newClient(new ClientConfig().register(JsonProcessingFeature.class).property(
+                                        JsonGenerator.PRETTY_PRINTING, true));
+        client.register(LoggingFilter.class);
 
-		Response postResponse = target.request(MediaType.APPLICATION_JSON)
-				.post(Entity.entity(model.toString(),
-						MediaType.APPLICATION_JSON));
+        WebTarget target = client.target(buildUrl("boards"));
 
-		System.out.println(postResponse);
-	}
+        Response postResponse = target.request(MediaType.APPLICATION_JSON).post(
+                                        Entity.entity(model.toString(), MediaType.APPLICATION_JSON));
 
-	private static String buildUrl(final String typeName) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("https://api.trello.com/1/").append(typeName + "?")
-				.append("key=" + TRELLO_KEY).append("&token=" + TRELLO_TOKEN);
+        JsonObject responseJson = postResponse.readEntity(JsonObject.class);
+        System.out.println(responseJson.getString("id"));
+    }
 
-		return sb.toString();
-	}
+    private static String buildUrl(final String typeName) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("https://api.trello.com/1/").append(typeName + "?").append("key=" + TRELLO_KEY)
+                                        .append("&token=" + TRELLO_TOKEN);
+
+        return sb.toString();
+    }
 
 }
